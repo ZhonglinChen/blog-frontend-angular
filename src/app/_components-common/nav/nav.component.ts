815945ff0add9eddf5617f2cee31db0c3ef8b985
unknown <zche294@aucklanduni.ac.nz>
2020-06-25 19:05:55 +1200

    dev

--- src/app/_components-common/nav/nav.component.ts
+++ src/app/_components-common/nav/nav.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -10,12 +9,8 @@ import { AuthService } from 'src/app/_services/auth.service';
 })
 export class NavComponent implements OnInit {
 
-  constructor(public authService: AuthService, private router: Router) { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
-  }
-  logout() {
-    this.authService.logout();
-    this.router.navigate(['/']);
   }
 }
